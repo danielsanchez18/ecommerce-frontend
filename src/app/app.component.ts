5668f20efdae64fa71fcbc2e61db31b2b484c5da
danielsanchez18 <dsanchez151r@gmail.com>
2024-07-29 16:28:29 -0500

    initial project configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { IStaticMethods } from 'preline/preline';
+
+declare global {
+  interface Window {
+    HSStaticMethods : IStaticMethods;
+  }
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +19,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ecommerce-frontend';
+
+  constructor (private router : Router) { }
+
+  ngOnInit () {
+    this.router.events.subscribe((event : Event) => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          window.HSStaticMethods.autoInit();
+        }, 100);
+      }
+    })
+  }
+
 }
